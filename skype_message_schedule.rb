d63b92923af7586e3ee6010b83cd65f283b4face
46goma <red4193@gmail.com>
2013-03-19 01:55:31 +0900

    n分間待ってから答えを聞くbotを用意した

--- skype_message_schedule.rb
+++ skype_message_schedule.rb
@@ -3,6 +3,7 @@
 require "rubygems"
 require "rb-skypemac"
 require "readline"
+require 'shellwords'
 
 include SkypeMac
 
@@ -24,13 +25,10 @@ if ARGV[0].nil?
   puts '上記の中からbotを潜入させたいchat_id(ex.#hoge/$fuga;xxxxxxx)を第一引数に指定してください'
   exit
 end
-ARGV[1] ||= 'www'
+time_limit = ARGV[1].to_i
+time_limit = 3 if (time_limit<=0)
 
-chat_id = search_last_chat_id_in_group()
-while true
-  latest_chat_id = search_last_chat_id_in_group()
-  puts "chatmessage #{ARGV[0]} #{ARGV[1]} unless chat_id == latest_chat_id"
-  puts Skype.send_(:command => "chatmessage #{ARGV[0]} #{ARGV[1]}") unless chat_id == latest_chat_id
-  chat_id = search_last_chat_id_in_group()
-  sleep(rand(100))
-end
+puts "chatmessage #{ARGV[0]} #{time_limit}分間だけ待ってやろう"
+puts Skype.send_(:command => "chatmessage #{ARGV[0]} #{time_limit}分間だけ待ってやろう")
+sleep(time_limit*60)
+puts Skype.send_(:command => "chatmessage #{ARGV[0]} さあ答えを聞こうじゃないか")
